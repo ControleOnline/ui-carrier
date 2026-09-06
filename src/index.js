f751a346a686126fb04300ca5e29f25d5073c372
Luiz Kim <luiz.kim@controleonline.com>
2026-09-06 12:34:01 -0300

    test: document ui-carrier people smoke flow

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 const moduleEntry = {};
 
 export default moduleEntry;
+// fluxo: transportador-cadastro | etapa: carrier-module-entry | wiki: https://github.com/ControleOnline/app-community/wiki/Venda-Producao
